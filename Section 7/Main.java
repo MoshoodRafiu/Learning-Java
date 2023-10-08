a3229ade768165f4f301e7dd06bcaffa61030c7d
Rafiu Moshood <meezyloaded@gmail.com>
2023-10-08 15:37:23 +0100

    learn: working with copy constructor

--- Section 7/Main.java
+++ Section 7/Main.java
@@ -3,11 +3,17 @@ public class Main {
     Car nissan = new Car("Nissan", 10000, 2020, "Green");
     Car dodge = new Car("Dodge", 11000, 2019, "Blue");
 
+    Car nissan2 = new Car(nissan);
+
+    nissan2.setMake("Nissan 2");
+
     nissan.setColor("Jet Black");
     dodge.setColor("Jet Black");
 
     nissan.setPrice(nissan.getPrice() / 2);
     dodge.setPrice(dodge.getPrice() / 2);
+
+    System.out.println(nissan2.getMake());
 
     System.out.println("Ths " + nissan.getMake() + " is worth $" + nissan.getPrice() + ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
     System.out.println("Ths " + dodge.getColor() + " is worth $" + dodge.getPrice() + ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
