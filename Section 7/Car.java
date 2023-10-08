d4c07481430e1e778aa3f55a6cd24bf7b31fc928
Rafiu Moshood <meezyloaded@gmail.com>
2023-10-08 15:25:35 +0100

    learn: working with setters

--- Section 7/Car.java
+++ Section 7/Car.java
@@ -4,36 +4,42 @@ public class Car {
   private int year;
   private String color;
 
-  public Car(String make, double price, int year, String color)
-  {
+  public Car(String make, double price, int year, String color) {
     this.make = make;
     this.price = price;
     this.year = year;
     this.color = color;
   }
 
-  public String getMake()
-  {
+  public String getMake() {
     return make;
   }
 
-  public double getPrice()
-  {
+  public void setMake(String make) {
+    this.make = make;
+  }
+
+  public double getPrice() {
     return price;
   }
 
-  public int getYear()
-  {
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public int getYear() {
     return year;
   }
 
-  public String getColor()
-  {
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  public String getColor() {
     return color;
   }
 
-  public void drive()
-  {
-
+  public void setColor(String color) {
+    this.color = color;
   }
 }
--- Section 7/Main.java
+++ Section 7/Main.java
@@ -1,8 +1,13 @@
 public class Main {
   public static void main(String[] args) {
-    Car nissan = new Car("Nissan", 10000, 2020, "green");
+    Car nissan = new Car("Nissan", 10000, 2020, "Green");
+    Car dodge = new Car("Dodge", 11000, 2019, "Blue");
 
-    Car dodge = new Car("Dodge", 11000, 2019, "blue");
+    nissan.setColor("Jet Black");
+    dodge.setColor("Jet Black");
+
+    nissan.setPrice(nissan.getPrice() / 2);
+    dodge.setPrice(dodge.getPrice() / 2);
 
     System.out.println("Ths " + nissan.getMake() + " is worth $" + nissan.getPrice() + ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
     System.out.println("Ths " + dodge.getColor() + " is worth $" + dodge.getPrice() + ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
