653849163a2fae70dc9751582d02f099a048be54
Rafiu Moshood <meezyloaded@gmail.com>
2023-10-08 16:54:05 +0100

    learn: escaping the reference trap

--- Section 7/Main.java
+++ Section 7/Main.java
@@ -1,7 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    Car nissan = new Car("Nissan", 10000, 2020, "Green");
-    Car dodge = new Car("Dodge", 11000, 2019, "Blue");
+    String[] spareParts = new String[] { "Tires", "Keys" };
+
+    Car nissan = new Car("Nissan", 10000, 2020, "Green", spareParts);
+    Car dodge = new Car("Dodge", 11000, 2019, "Blue", spareParts);
 
     nissan.drive();
     dodge.drive();
